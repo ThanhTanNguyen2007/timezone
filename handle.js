dc89a4138769f766623fd64ee02892f944aeb478
Nguyen Thanh Tan <tan.187pm20569@vanlanguni.vn>
2021-09-20 08:17:13 +0700

    fix json file

--- handle.js
+++ handle.js
@@ -9,7 +9,7 @@ const newData = data.map((x) => {
     tzCode: x.slice(index + 2),
     name: x
       .slice(index + 2)
-      .replace("/", "_")
+      .replace(/\//g, "_")
       .replace(" ", "_")
       .replace("-", "_"),
     utc: x.slice(1, index),
