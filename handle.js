243cfb02bdcfaff2a8d892057d3a56043d1d0560
Nguyen Thanh Tan <tan.187pm20569@vanlanguni.vn>
2021-10-18 16:09:43 +0700

    change tz

--- handle.js
+++ handle.js
@@ -6,13 +6,17 @@ const newData = data.map((x) => {
   const index = x.indexOf(")");
   return {
     label: x,
-    tzCode: x.slice(index + 2),
+    tzCode: x
+      .slice(index + 2)
+      .replace(" ", "_")
+      .replace("GMT+", "GMT_PLUS_")
+      .replace("GMT-", "GMT_MINUS_"),
     name: x
       .slice(index + 2)
       .replace(/\//g, "_")
       .replace(" ", "_")
       .replace("-", "_"),
-    utc: x.slice(1, index),
+    utc: x.slice(4, index),
   };
 });
 
